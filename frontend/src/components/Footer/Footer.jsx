533e0e4ecd1d33ffa81b9ad3e8a5ecf6e1109f38
shivam151124 <shivambehera12@gmail.com>
2025-10-05 21:53:36 +0530

    footer logo changes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
        <div className="footer-content">
           <div className="footer-content-left">
-              <img src={assets.logo} alt="" />
+              <img src={assets.logo} alt="" id="footer-logo"/>
               <p>Good food starts with great choices. Explore our menu and choose from a diverse menu filled with handpicked dishes made to please every taste bud.</p>
               <div className="footer-social-icons">
                   <img src={assets.facebook_icon} alt="" /> 
