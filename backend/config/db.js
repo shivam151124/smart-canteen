71637ed4ff74cf875fa1e560598d977bb79bce78
shivam151124 <shivambehera12@gmail.com>
2025-10-07 09:01:58 +0530

    comment add

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,4 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://shivambehera:01112004@cluster0.e6tktn3.mongodb.net/food-del').then(()=>console.log("DB Connected"));
 }
+
